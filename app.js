85e2f2753f7e12ea6b465169bb28094d5a259f55
ZainabAamir <zaamir427@gmail.com>
2026-07-06 17:13:15 +0500

    practise

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+//first line of code in javaScript
 console.log("hello world");
  
+//understanding data types and Variables in javaScript
 var myName = "zainabaamirkhan"
 console.log(myName)
 
@@ -8,6 +10,26 @@ console.log(ourName)
 
 const pi = 3.14
 console.log(pi)
+
+//initializing variables with assignment operator. 
+var numberOfPhones = 3
+var amountOfPhones = 500000
+var companyOfPhones = 'Samsung'
+
+console.log(numberOfPhones + "\n" + amountOfPhones + "\n" + companyOfPhones)
+
+var a;
+var b;
+var c;
+
+a = 20, b = 30, c = 40
+d = a+b+c
+console.log(d)
+
+
+
+
+
 
 
 
