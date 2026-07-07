c41a0921395c38d6da244eea9119f67f35e76e00
ZainabAamir <zaamir427@gmail.com>
2026-07-07 15:14:24 +0500

    changes

--- app1.js
+++ app1.js
@@ -10,3 +10,15 @@ c = b + 10
 console.log(a)
 console.log(b)
 console.log(c)
+
+//JavaScript is a case sensitive language
+var camelCase = "string"
+var snake_case = "snake"
+var snakeCase = "case"
+var camel_case = "camel"
+
+console.log(camelCase)
+console.log(snakeCase)
+console.log(camel_case)
+console.log(snake_case)
+
