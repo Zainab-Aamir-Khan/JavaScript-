5d3dabc46d6ac827ce9803c7a47681e80c6e8d0b
ZainabAamir <zaamir427@gmail.com>
2026-07-08 09:58:35 +0500

    practise

--- script.js
+++ script.js
@@ -19,6 +19,23 @@
 // console.log(b)
 // typeof b
 
+//understaning object type. 
+let name = {
+    student : "safa",
+    age : 2
+}
 
+console.log(name)
+ 
+//to print any specific key in the object
+console.log(name["age"])
+console.log(name.student)
 
+//to change any value of a particular key inside the object 
+name["student"] = "sana"
+console.log(name.student)
+console.log(name)
 
+name["age"] = name["age"] + 1
+console.log(name)
+console.log(typeof name)
