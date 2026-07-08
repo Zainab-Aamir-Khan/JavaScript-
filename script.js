40276a5999c0da915a77f5d776b2e19b5a02592a
ZainabAamir <zaamir427@gmail.com>
2026-07-08 09:03:04 +0500

    ..

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-var x = undefined
-var y = null
-var z = true
-var a = false
+// var x = undefined
+// var y = null
+// var z = true
+// var a = false
 
-console.log(x,y, z, a);
+// console.log(x,y, z, a);
 
 
-console.log("hello world");
-console.log('hi my name is javascript...')
+// console.log("hello world");
+// console.log('hi my name is javascript...')
+
+// let a = "Zainab"
+// a = "sana"
+
+// console.log(a)
+
+// const b = "Zainab"
+// // b = "safa" cant be reinitialised, cuz this is constant. 
+// console.log(b)
+// typeof b
+
+
+
+
