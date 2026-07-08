703b57cd6dc392a8b42a56d1ce9197dd8f1827a3
ZainabAamir <zaamir427@gmail.com>
2026-07-08 11:18:06 +0500

    .

---
+++ variables and data types.js
@@ -0,0 +1,48 @@
+// //Quesion number 1
+
+// let product = {
+//     name : "pen",
+//     rate : 4,
+//     price : 100,
+//     isDealOfDay : true 
+// }
+
+// console.log(product)
+// console.log(typeof product)
+// console.log(product.isDealOfDay)
+// console.log(typeof product["name"])
+
+// //Question number 2
+// const profile = {
+//     name : "Pleasedontopenit",
+//     isFollow : false,
+//     posts : 100,
+//     followers : 100,
+//     following : 1,
+//     bio : "my day = 5 june"
+// }
+
+// console.log(profile);
+
+let myBio = {
+    name : "zainab",
+    age : 23,
+    city : "Karachi",
+    isStudent : true
+} 
+
+console.log(myBio)
+
+a = "javascript";
+b = 2026;
+c = true;
+d = undefined
+e = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+
+
