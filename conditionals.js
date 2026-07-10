295bf774ca22592d90df924a2016cefccbb3b52e
ZainabAamir <zaamir427@gmail.com>
2026-07-10 11:38:10 +0500

    condirtionls

--- conditionals.js
+++ conditionals.js
@@ -14,3 +14,41 @@ if (a !== b){
 else{
     console.log(false);
 }
+
+if (b > a){
+    console.log("b is greater")
+}
+else{
+    console.log("A is greater");
+}
+
+if (a > b ){
+    console.log("I will work from tomorrow");
+}
+else{
+    console.log("I'll work from todayyyyyy");
+}
+
+
+if (a  = 12){
+    console.log("a is fine but...");
+}
+else if(a = 13){
+    console.log("b is better");
+}
+else{
+    console.log("all are fine");
+}
+
+let marks = 20;
+
+if (marks == 100){
+    if (marks >= 80 ){
+        console.log("topper");
+    }
+    else{
+         console.log("passed")
+    }
+}else{
+    console.log("failed badly")
+}
