9b0370e04f33413335045ab1eac030390cdaf031
ZainabAamir <zaamir427@gmail.com>
2026-07-10 11:15:17 +0500

    ..

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,16 @@
 //Conditional statement
+let a = 12;
+if (age = 11){
+    console.log("He is a kiddo");
+}
+else{
+    console.log("not a kiddo anymore HAHAHA");
+}
+
+let b = 13;
+if (a !== b){
+    console.log("true");
+}
+else{
+    console.log(false);
+}
