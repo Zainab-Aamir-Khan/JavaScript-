6df07fa6da8eed21e0edb0b33d888de65559ffc9
ZainabAamir <zaamir427@gmail.com>
2026-07-13 12:05:42 +0500

    ..

---
+++ switch.js
@@ -0,0 +1,16 @@
+// switch
+let city = "Karchi";
+
+switch (city) {
+    case "Lahore":
+        console.log("hello Lahore");
+        break;
+    case "Isloo":
+        console.log("Hello Islamabad");
+        break;
+    case "Karachi":
+        console.log("Hello Khi");
+        break;
+    default:
+        console.log("No entry");
+}
